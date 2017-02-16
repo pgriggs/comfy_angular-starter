13a3feedd1637a0ac9a74f94c6f89d3f988aad5d
Peter Griggs <griggs@Peters-MacBook-Pro-2.local>
2017-02-16 00:50:33 -0800

    more gulp production

--- Gulpfile.js
+++ Gulpfile.js
@@ -10,7 +10,7 @@ var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 
 
-// Concatenate & Minify JS
+// Concatenate & Minify JS for production build
 gulp.task('scripts', function() {
     return gulp.src('app/js/**/*.js')
       .pipe(concat('main.js'))
@@ -19,23 +19,38 @@ gulp.task('scripts', function() {
         .pipe(gulp.dest('build/js'));
 });
 
-// image optimization
+// image optimization for production build
  gulp.task('images', function() {
   return gulp.src('app/assets/*')
     .pipe(cache(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true })))
     .pipe(gulp.dest('build/assests/img'));
 });
 
+ // Styles task for production build
+gulp.task('stylesProd', function() {
+    gulp.src('build/sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('build/css/'))
+        .pipe(livereload());
+});
+
+// bower install to gulp task for production build
+gulp.task('bowerProd', function() {
+  return bower('./bower_components')
+    .pipe(gulp.dest('build/lib/'))
+});
+
+// production build task
+gulp.task('production', ['bowerProd'], function() {
+    gulp.watch('build/sass/**/*.scss',['stylesProd']);
+    gulp.run('scripts');
+    gulp.run('images');
+});
+
 // bower install to gulp task
 gulp.task('bower', function() {
   return bower('./bower_components')
     .pipe(gulp.dest('app/lib/'))
-});
-
-// bower install to gulp task
-gulp.task('bowerProd', function() {
-  return bower('./bower_components')
-    .pipe(gulp.dest('build/lib/'))
 });
 
 // Styles task
@@ -54,13 +69,6 @@ gulp.task('connect', function() {
     livereload: true,
     fallback: 'app/index.html'
   });
-});
-
-// production build task
-gulp.task('production', ['bowerProd'], function() {
-    gulp.watch('build/sass/**/*.scss',['styles']);
-    gulp.run('scripts');
-    gulp.run('images');
 });
 
 // Watch task 
